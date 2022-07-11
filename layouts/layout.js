339e874cd463dd10d404dbc5c7b3c1f53b6fe0c0
louis <louisborges.h@gmail.com>
2022-07-11 23:56:26 +0800

    change some image style

--- layouts/layout.js
+++ layouts/layout.js
@@ -42,7 +42,7 @@ const Layout = ({
                   alt={BLOG.author}
                   width={24}
                   height={24}
-                  src="../public/Uranus.jpg"
+                  src={`https://gravatar.com/avatar/${emailHash}`}
                   className="rounded-full"
                 />
                 <p className="ml-2 md:block">{BLOG.author}</p>
