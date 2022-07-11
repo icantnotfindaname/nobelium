18268d8721647eaef2e67bf2282ea13a752ed0a0
louis <louisborges.h@gmail.com>
2022-07-11 21:44:59 +0800

    change some image style

--- components/Header.js
+++ components/Header.js
@@ -76,6 +76,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
                   viewBox="0 0 24 24"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
+                  xmlns:xlink="http://www.w3.org/1999/xlink"
                 >
                   {/* <rect width="24" height="24" fill="url(#paint0_svg)" /> */}
                   <defs>
@@ -99,7 +100,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
                     className="fill-current text-black dark:text-white"
                     fill="url(#paint0_svg)"
                   />
-
+                  
                 </svg>
               </div>
             </a>
