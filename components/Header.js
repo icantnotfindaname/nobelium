748c9e628adf19cbdbc8370dc2f55e8dd21f083c
louis <louisborges.h@gmail.com>
2022-07-11 20:58:22 +0800

    change header style

--- components/Header.js
+++ components/Header.js
@@ -77,11 +77,11 @@ const Header = ({ navBarTitle, fullWidth }) => {
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <rect
+                  {/* <rect
                     width="24"
                     height="24"
                     className="fill-current text-black dark:text-white"
-                  />
+                  /> */}
                   <rect width="24" height="24" fill="url(#paint0_svg)" />
                   <defs>
                     {/* <radialGradient
